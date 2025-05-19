56261b60aafdae1e14da0bdb3cef8426a6ab56f6
juanposada25 <juandiegoposada245@gmail.com>
2025-05-18 21:41:50 -0500

    se creo el index de comuna

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,27 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Comunas from '@/views/Comunas.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/comunas',
+    name: 'Comunas',
+    component: Comunas
+  },
+    {
+    path: '/editar-comuna/:id',
+    name: 'EditarComuna',
+    component: EditarComuna
+  },
+  {
+    path: '/add-comuna/',
+    name: 'NewComuna',
+    component: NewComuna
   },
   {
     path: '/about',
