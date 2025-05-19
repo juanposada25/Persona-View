d1be23cb248c3af64dee028615dd34edc703eaf2
juanposada25 <juandiegoposada245@gmail.com>
2025-05-18 23:28:33 -0500

    cambio navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
     <router-link to="/comunas">Comunas</router-link> |
     <router-link to="/municipios">Municipios</router-link> |
     <router-link to="/departamentos">Departamentos</router-link> |
